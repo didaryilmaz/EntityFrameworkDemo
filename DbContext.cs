b474d489b9b8e91948195b4afee03d55e999f915
Didar Yılmaz <didary24@gmail.com>
2025-03-03 11:49:36 +0300

    load project

---
+++ DbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace EntityFrameworkDemo
+{
+    public class DbContext
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthWindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server");
+        }
+
+}
